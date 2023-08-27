7db2c76043185e55f3754c23f855ab2748cf91b9
Ekaterina Kononova <ekaterina.panova@mail.ru>
2023-08-27 20:06:25 +0500

    changed Main

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,16 @@ public class Main {
     static BlockingQueue<String> letterA = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> letterB = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> letterC = new ArrayBlockingQueue<>(100);
-    static int aMax = 0;
-    static  String maxLineA = null;
-    static int bMax = 0;
-    static  String maxLineB = null;
-    static int cMax = 0;
-    static  String maxLineC = null;
+
+    static final int AMOUNT_OF_TEXTS = 10_000;
 
     public static void main(String[] args) throws InterruptedException {
 
         new Thread(() -> {
-            for (int i = 0; i < 10_000; i++) {
+            for (int i = 0; i < AMOUNT_OF_TEXTS; i++) {
                 String text = generateText("abc", 100_000);
                 try {
-                    letterA.put(text); // можно в один трай-кетч или лучше для каждого свой?
+                    letterA.put(text);
                     letterB.put(text);
                     letterC.put(text);
                 } catch (InterruptedException e) {
@@ -29,70 +25,31 @@ public class Main {
         }).start();
 
         // а
-        Thread threadA = new Thread(() -> {
-            for (int i = 0; i < 10_000; i++) {
-                String line;
-                int countA;
-                try {
-                    line = letterA.take();
-                    countA = countLettersInOneLine(line, 'a');
-                } catch (InterruptedException e) {
-                    return;
-                }
-                if (countA > aMax) {
-                    aMax = countA;
-                    maxLineA = line;
-                }
+        new Thread(() -> {
+            try {
+                countLettersInThread(AMOUNT_OF_TEXTS, letterA, 'a');
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        });
-        threadA.start();
+        }).start();
 
         // b
-        Thread threadB = new Thread(() -> {
-            for (int i = 0; i < 10_000; i++) {
-                String line;
-                int countB;
-                try {
-                    line = letterB.take();
-                    countB = countLettersInOneLine(line, 'b');
-                } catch (InterruptedException e) {
-                    return;
-                }
-                if (countB > bMax) {
-                    bMax = countB;
-                    maxLineB = line;
-                }
+        new Thread(() -> {
+            try {
+                countLettersInThread(AMOUNT_OF_TEXTS, letterB, 'b');
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        });
-        threadB.start();
+        }).start();
 
         // c
-        Thread threadC = new Thread(() -> {
-            for (int i = 0; i < 10_000; i++) {
-                String line;
-                int countC;
-                try {
-                    line = letterC.take();
-                    countC = countLettersInOneLine(line, 'c');
-                } catch (InterruptedException e) {
-                    return;
-                }
-                if (countC > cMax) {
-                    cMax = countC;
-                    maxLineC = line;
-                }
+        new Thread(() -> {
+            try {
+                countLettersInThread(AMOUNT_OF_TEXTS, letterC, 'c');
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        });
-        threadC.start();
-
-        threadA.join();
-        threadB.join();
-        threadC.join();
-
-        System.out.println("Максимальное кол-во 'а' равно " + aMax + " и содержится в строке: \n" + maxLineA); // отображает
-                                                                // только первую встретившуюся строку с максимальным кол-вом
-        System.out.println("Максимальное кол-во 'b' равно " + bMax + " и содержится в строке: \n" + maxLineB);
-        System.out.println("Максимальное кол-во 'c' равно " + cMax + " и содержится в строке: \n" + maxLineC);
+        }).start();
     }
 
     public static String generateText(String letters, int length) {
@@ -104,13 +61,24 @@ public class Main {
         return text.toString();
     }
 
-    public static int countLettersInOneLine(String line, char c) throws InterruptedException {
-        int a = 0;
-        for (int j = 0; j < line.length(); j++) {
-            if (line.charAt(j) == c) {
-                a++;
+    public static void countLettersInThread (int amountOfTexts, BlockingQueue<String> letter, char c) throws InterruptedException {
+        String lineMax = null;
+        int charMax = 0;
+        for (int i = 0; i < amountOfTexts; i++) {
+            String line;
+            int countChar = 0;
+            line = letter.take();
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == c) {
+                    countChar++;
+                }
+            }
+            if (countChar > charMax) {
+                charMax = countChar;
+                lineMax = line;
             }
         }
-        return a;
+        System.out.println("Максимальное кол-во '" + c +"' равно " + charMax + " и содержится в строке: \n" + lineMax); // отображает
+        // только первую встретившуюся строку с максимальным кол-вом
     }
 }
